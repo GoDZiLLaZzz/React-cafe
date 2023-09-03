548a88de5ce0fda4bf918d3373d03ab97a596c4b
Lion <luhsm1974@gmail.com>
2023-09-03 18:47:29 +0300

    First commit

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer/Footer'
+import { Outlet } from 'react-router-dom';
+
+const MainLayout: React.FC = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Outlet />
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
